eff6b90b9d7e5070e773ec6607236ce73ce56422
Sim Yue Yang <yueyangsim02@gamil.com>
2023-03-11 21:45:17 +0800

    Chapter4.2

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,63 @@
 mod utils;
 
 use image::{ImageBuffer, Rgb, RgbImage};
+use utils::ray::Ray;
+use utils::vec3::{Color, Point3, Vec3};
+
+fn ray_color(r: &Ray) -> Color {
+    let unit_direction = r.direction().unit_vector();
+    let t = 0.5 * (unit_direction.y() + 1.0);
+    (1.0 - t) * Color { e: [1.0, 1.0, 1.0] } + t * Color { e: [0.5, 0.7, 1.0] }
+}
+
+fn write_color(color: &Color) -> Rgb<u8> {
+    Rgb([
+        (color[0] * 255.0) as u8,
+        (color[1] * 255.0) as u8,
+        (color[2] * 255.0) as u8,
+    ])
+}
 
 fn main() {
-    const IMAGE_WIDTH: u32 = 256;
-    const IMAGE_HEIGHT: u32 = 256;
+    // IMAGE
+    const ASPECT_RATIO: f64 = 16.0 / 9.0;
+    const IMAGE_WIDTH: u32 = 400;
+    const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32;
+
+    // CAMERA
+    const VIEWPORT_HEIGHT: f64 = 2.0;
+    const VIEWPORT_WIDTH: f64 = ASPECT_RATIO * VIEWPORT_HEIGHT;
+    const FOCAL_LENGTH: f64 = 1.0;
+
+    // VIEWPORT
+    let origin = Point3 { e: [0.0, 0.0, 0.0] };
+    let horizontal = Point3 {
+        e: [VIEWPORT_WIDTH, 0.0, 0.0],
+    };
+    let vertical = Point3 {
+        e: [0.0, VIEWPORT_HEIGHT, 0.0],
+    };
+    let lower_left_corner = origin
+        - horizontal / 2.0
+        - vertical / 2.0
+        - Point3 {
+            e: [0.0, 0.0, FOCAL_LENGTH],
+        };
+
+    // RENDER
     let mut imgbuf: RgbImage = ImageBuffer::new(IMAGE_WIDTH, IMAGE_HEIGHT);
-
     for (i, j, pixel) in imgbuf.enumerate_pixels_mut() {
-        let r: f64 = i as f64 / (IMAGE_WIDTH - 1) as f64;
-        let g: f64 = (IMAGE_HEIGHT - 1 - j) as f64 / (IMAGE_HEIGHT - 1) as f64;
-        let b = 0.25;
+        let u: f64 = i as f64 / (IMAGE_WIDTH - 1) as f64;
+        let v: f64 = (IMAGE_HEIGHT - 1 - j) as f64 / (IMAGE_HEIGHT - 1) as f64;
 
-        let ir = (r* 255.0) as u8;
-        let ig = (g * 255.0) as u8;
-        let ib = (b * 255.0) as u8;
+        let r = Ray {
+            origin,
+            direction: lower_left_corner + u * horizontal + v * vertical - origin,
+        };
+        let color = ray_color(&r);
 
-        *pixel = Rgb([ir, ig, ib]);
+        *pixel = write_color(&color);
     }
 
-    imgbuf.save("1.png").unwrap();
+    imgbuf.save("image/1.png").expect("Image cannot be saved.");
 }
--- src/utils/ray.rs
+++ src/utils/ray.rs
@@ -1,21 +1,21 @@
 use crate::utils::vec3::{Vec3, Point3};
 
 #[derive(Debug, Copy, Clone)]
-struct Ray {
+pub struct Ray {
     pub origin: Point3,
     pub direction: Vec3,
 }
 
 impl Ray {
-    fn origin (&self) -> Point3 {
+    pub fn origin (&self) -> Point3 {
         self.origin
     }
 
-    fn direction (&self) -> Point3 {
+    pub fn direction (&self) -> Point3 {
         self.direction
     }
 
-    fn at (&self, t:f64) -> Point3 {
+    pub fn at (&self, t:f64) -> Point3 {
         self.origin + t * self.direction
     }
 }

12adeb0ba17d57d765ff87487d277f064d318b31
Sim Yue Yang <yueyangsim02@gamil.com>
2023-03-12 16:29:10 +0800

    Chapter5.2

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,15 @@ use utils::ray::Ray;
 use utils::vec3::{Color, Point3, Vec3};
 
 fn ray_color(r: &Ray) -> Color {
+    if hit_sphere(
+        &Point3 {
+            e: [0.0, 0.0, -1.0],
+        },
+        0.5,
+        &r,
+    ) {
+        return Color { e: [1.0, 0.0, 0.0] };
+    }
     let unit_direction = r.direction().unit_vector();
     let t = 0.5 * (unit_direction.y() + 1.0);
     (1.0 - t) * Color { e: [1.0, 1.0, 1.0] } + t * Color { e: [0.5, 0.7, 1.0] }
@@ -16,6 +25,24 @@ fn write_color(color: &Color) -> Rgb<u8> {
         (color[1] * 255.0) as u8,
         (color[2] * 255.0) as u8,
     ])
+}
+
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+    // (x − Cx)^2 + (y − Cy)^2 + (z − Cz)^2 = (P(t)−C)^2
+    // (P(t) − C)^2 = r^2
+    // (A + tb − C)^2 = r^2
+    // (tb + A − C)^2 = r^2
+    // (tb)^2 + 2tb⋅(A−C) + (A−C)^2 - r^2 = 0
+    // Solve for tb
+    // A- C is origin minus center, tb is direction
+
+    let oc = r.origin() - *center;
+    let a = r.direction().dot(&r.direction());
+    let b = 2.0 * r.direction().dot(&oc);
+    let c = oc.dot(&oc) - radius * radius;
+    let discriminant = b * b - 4.0 * a * c;
+
+    discriminant >= 0.0
 }
 
 fn main() {
